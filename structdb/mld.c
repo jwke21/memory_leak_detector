cb30257720191348d41a8776b90f00d73c5563c9
jwke21 <jakevm21@gmail.com>
2022-06-30 22:26:33 -0700

    Implemented print functions to display db

--- structdb/mld.c
+++ structdb/mld.c
@@ -23,21 +23,39 @@ char *d_type_to_string(data_type_t d_type)
 /*
    Prints all info about a given field.
 */
-void print_field(field_info_t *fld_info)
+void print_field(field_info_t fld_info)
 {
-    printf("Field name:      \t\t%s", fld_info->fname);
-    printf("Field size:      \t\t%ul", fld_info->size);
-    printf("Field offset:    \t\t%ul", fld_info->offset);
-    printf("Field data type: \t\t%s", d_type_to_string(fld_info->dtype));
-    printf("Nested struct:   \t\t%s", fld_info->nested_str_name);
+    printf("Field name:      \t\t%s", fld_info.fname);
+    printf("Field size:      \t\t%ul", fld_info.size);
+    printf("Field offset:    \t\t%ul", fld_info.offset);
+    printf("Field data type: \t\t%s", d_type_to_string(fld_info.dtype));
+    printf("Nested struct:   \t\t%s", fld_info.nested_str_name);
 }
 
 void print_structure_rec(struct_db_rec_t *struct_rec)
 {
     if (!struct_rec) return;
 
+    printf("Structure Name:   \t%s\n\n", struct_rec->struct_name);
+    printf("Structure Size:   \t%ul\n", struct_rec->ds_size);
+    printf("Number of Fields: \t%ul\n", struct_rec->n_fields);
+
     int i;
     for (i = 0; i < struct_rec->n_fields; ++i) {
-        
+        printf("\t\tFIELD %d\n", i);
+        print_field(struct_rec->fields[i]);
     }
 }
+
+void print_structure_db(struct_db_t *struct_db)
+{
+    struct_db_rec_t *cur_rec;
+    int i;
+
+    cur_rec = struct_db;
+    for (i = 0; i < struct_db->count; ++i) {
+        print_structure_rec(cur_rec);
+        cur_rec = cur_rec->next;
+    }
+}
+
